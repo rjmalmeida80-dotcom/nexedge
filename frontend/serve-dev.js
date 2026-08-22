99c9f3f661d62e833f0ff7f321f3ed028598c516
Ricardo <ricardo@nexedge.pt>
2026-08-22 20:52:03 +0100

    MediaLoop + Senhas: 4 frontends premium - admin, operador, quiosque, ecra TV

--- frontend/serve-dev.js
+++ frontend/serve-dev.js
@@ -29,6 +29,10 @@ const ESPECIAIS = {
   '/relatorios-bi/':     path.join(__dirname, 'relatorios-bi.html'),
   '/projectos':          path.join(__dirname, 'projectos.html'),
   '/logistica': path.join(__dirname, 'logistica.html'),
+  '/medialoop-admin': path.join(__dirname, 'medialoop-admin.html'),
+  '/senhas-admin': path.join(__dirname, 'senhas-admin.html'),
+  '/quiosque': path.join(__dirname, 'quiosque.html'),
+  '/ecra-senhas': path.join(__dirname, 'ecra-senhas.html'),
   '/logistica/': path.join(__dirname, 'logistica.html'),
   '/projectos/':         path.join(__dirname, 'projectos.html'),
 };
