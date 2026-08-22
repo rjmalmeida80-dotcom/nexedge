f496915b2ba1692cbb60c88c48542ad51a33c86b
Ricardo <ricardo@nexedge.pt>
2026-08-22 21:48:08 +0100

    Landing page premium + Simulador frete + 508 linhas

--- frontend/serve-dev.js
+++ frontend/serve-dev.js
@@ -32,6 +32,8 @@ const ESPECIAIS = {
   '/wms': path.join(__dirname, 'wms.html'),
   '/torre-controlo': path.join(__dirname, 'torre-controlo.html'),
   '/motorista-app': path.join(__dirname, 'motorista-app.html'),
+  '/landing': path.join(__dirname, 'landing.html'),
+  '/frete-simulator': path.join(__dirname, 'frete-simulator.html'),
   '/wms/': path.join(__dirname, 'wms.html'),
   '/medialoop-admin': path.join(__dirname, 'medialoop-admin.html'),
   '/senhas-admin': path.join(__dirname, 'senhas-admin.html'),
